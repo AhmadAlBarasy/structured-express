69f9303670177fba2008f601d2fe358ed1019a68
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-20 23:03:38 +0300

    Added support for AppRotute to the router routes

--- src/classes/Router.ts
+++ src/classes/Router.ts
@@ -2,6 +2,7 @@ import RouterOptions from "../interfaces/RouterOptions";
 import express, { Router as ExpressRouter } from 'express';
 import MethodsRoute from "./MethodsRoute";
 import { HTTPMethod } from "../types/types";
+import AppRoute from "./AppRoute";
 
 class RouterClass {
     private router: ExpressRouter;
@@ -14,13 +15,20 @@ class RouterClass {
         return this.router;
     }
     // attach routes and methods handlers into the router.
-    private setupRoutes(routes: MethodsRoute[]){
+    private setupRoutes(routes: (MethodsRoute | AppRoute)[]){
         routes.forEach(route => {
-            Object.keys(route.handler).forEach(method => {
-                const handlers = route.handler[method as HTTPMethod];
-                if (handlers)
-                    (this.router as any)[method](route.path, ...handlers);
-            })
+            if (route instanceof MethodsRoute){
+                Object.keys(route.handler).forEach(method => {
+                    const handlers = route.handler[method as HTTPMethod];
+                    if (handlers)
+                        (this.router as any)[method](route.path, ...handlers);
+                });
+            }
+            else if (route instanceof AppRoute){
+                if (Array.isArray(route.handler))
+                    this.router.use(route.path, ...route.handler);
+                this.router.use(route.path, route.handler);
+            }
         });
     }
 }
--- src/interfaces/RouterOptions.ts
+++ src/interfaces/RouterOptions.ts
@@ -1,6 +1,7 @@
 import { RouterOptions as ExpressRouterOptions } from "express";
 import MethodsRoute from "../classes/MethodsRoute";
+import AppRoute from "../classes/AppRoute";
 
 export default interface RouterOptions extends ExpressRouterOptions {
-    routes?: MethodsRoute[];
+    routes?: (MethodsRoute | AppRoute)[];
 }
