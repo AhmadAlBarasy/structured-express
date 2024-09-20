d4858006d89d15c9f77627618e252c78ed7b32ee
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-20 03:56:14 +0300

    exported type, interface, and class declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,38 @@
-import { Application } from "express";
+import {
+    Application,
+    RequestHandler,
+    Router,
+    RouterOptions as ExpressRouterOptions 
+    } from "express";
+import { PathParams } from 'express-serve-static-core';
+import MethodsRoute from "./classes/MethodsRoute";
+import AppRoute from './classes/AppRoute';
+import Middleware from './classes/Middleware';
+
 
 export interface serverConfig {
     port?: number;
+    routes?: AppRoute[];
+    middlewares?: Middleware[];
 }
 
 export function startServer (config: serverConfig): Application
 
+export type AppRequestHandler = RequestHandler | RequestHandler[] | Router;
+
+export type HTTPMethod = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'head' | 'options';
+
+// just an interface that will be extended to create classes that holds different kinds of routes.
+export default interface Route {
+    path: PathParams;
+}
+
+// the Partial utility is used to make all methods optional.
+export interface MethodsHandlers extends Partial<Record<HTTPMethod, RequestHandler[]>> {
+    // to handle dynamic methods
+    [key: string]: RequestHandler[] | undefined;
+}
+
+export default interface RouterOptions extends ExpressRouterOptions {
+    routes?: MethodsRoute[];
+}
